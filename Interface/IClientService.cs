84d854658f9342677d8304cdc09442fd98320bdc
VitorKubica <vitor05kubica12@gmail.com>
2024-11-22 15:59:09 -0300

    feat: new test

---
+++ Interface/IClientService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SolarSync_API.Models;
+using SolarSync_API.DTO;
+
+namespace SolarSync_API.Services
+{
+    public interface IClientService
+    {
+        Task<IEnumerable<Client>> GetClientsAsync();
+        Task<Client?> GetClientByIdAsync(string id);
+        Task<Client?> GetClientByCPFAsync(string cpf);
+        Task<Client?> UpdateClientAsync(string id, UpdateClientDTO clientDto);
+        Task<Client> CreateClientAsync(BaseClient clientDto);
+        Task<bool> DeleteClientAsync(string id);
+    }
+}
